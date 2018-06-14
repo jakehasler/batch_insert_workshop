1bb62f9880afb2c72d19086b3f223881f8865369
Jake Hasler <jakehasler@gmail.com>
2018-06-14 12:05:59 -0600

    Running GithubApi.parse() will do the thing

--- lib/batch_insert_workshop/services/github_api.ex
+++ lib/batch_insert_workshop/services/github_api.ex
@@ -1,63 +1,93 @@
 defmodule BatchInsertWorkshop.GithubApi do
-
   import Ecto.Query, warn: false
   alias BatchInsertWorkshop.Repo
-  alias BatchInsertWorkshop.{Error,Graphql}
+  alias BatchInsertWorkshop.Model.{Language, GitRepo, RepoLanguage}
+  alias BatchInsertWorkshop.{Error, Graphql, Payloads}
 
   @batch_size 10
 
   def repos(global_id \\ "") do
-    case Graphql.fetch(query(global_id), input(global_id)) do
-      {:ok, res} -> parse(res)
-      {:error, errors} -> Error.from_response(errors)
-    end
+    Payloads.GithubPayload.sample()
   end
 
-  def parse(res) do
-    IO.inspect res
+  def parse() do
+    inserted_langs = repos() |> languages |> Enum.map(&{&1.name, &1.id}) |> Enum.into(%{})
+    inserted_repos = repos() |> git_repos |> Enum.map(&{&1.name, &1.id}) |> Enum.into(%{})
 
-    res["page_info"]["end_cursor"]
-  end
-
-  defp query(global_id \\ "") do
-    after_id = case global_id do
-                "" -> ""
-                id -> "after: $after,"
-              end
-
-    after_filter = case global_id do
-                     "" -> ""
-                     _ -> ", $after:String!"
-                   end
-
-    """
-    query($first:Int!#{after_filter}){
-      organization(login: "Podium") {
-        repositories(first: $first, #{after_id}
-                    orderBy:{field: NAME, direction: ASC}) {
-          pageInfo{
-            endCursor
+    repo_langs =
+      repos()["nodes"]
+      |> Enum.flat_map(fn repo_object ->
+        get_in(repo_object, ["languages", "nodes"])
+        |> Enum.map(fn lang_object ->
+          %{
+            git_repo_id: inserted_repos[repo_object["name"]],
+            language_id: inserted_langs[lang_object["name"]],
+            inserted_at: DateTime.utc_now(),
+            updated_at: DateTime.utc_now()
           }
-          nodes{
-            name
-            url
-            languages(first: #{@batch_size}) {
-              nodes{
-                name
-              }
-            }
-          }
-        }
-      }
-    }
-    """
+        end)
+      end)
+
+    {_, data} =
+      Repo.insert_all(
+        RepoLanguage,
+        repo_langs,
+        on_conflict: :replace_all,
+        returning: true,
+        conflict_target: [:git_repo_id, :language_id]
+      )
+
+    data
   end
 
-  defp input(global_id \\ "") do
-    map = %{"first" => @batch_size}
-    if global_id != "" do
-      map = Map.put(map, "after", global_id)
-    end
-    map
+  def languages(res) do
+    langs =
+      res["nodes"]
+      |> Enum.flat_map(fn repo_object ->
+        get_in(repo_object, ["languages", "nodes"])
+      end)
+      |> Enum.uniq()
+      |> Enum.map(
+        &[
+          name: &1["name"],
+          inserted_at: DateTime.utc_now(),
+          updated_at: DateTime.utc_now()
+        ]
+      )
+
+    {_, data} =
+      Repo.insert_all(
+        Language,
+        langs,
+        on_conflict: :replace_all,
+        returning: true,
+        conflict_target: :name
+      )
+
+    data
+  end
+
+  def git_repos(res) do
+    repo_names =
+      res["nodes"]
+      |> Enum.map(
+        &[
+          name: &1["name"],
+          url: &1["url"],
+          inserted_at: DateTime.utc_now(),
+          updated_at: DateTime.utc_now()
+        ]
+      )
+
+    {_, data} =
+      Repo.insert_all(
+        GitRepo,
+        repo_names,
+        on_conflict: :replace_all,
+        returning: true,
+        conflict_target: :name
+      )
+
+    data
   end
 end
